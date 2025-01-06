66dcd2ce6e41c219bbb0d18dc3f097f7f8bbbb9a
xayliot <megaponch2004@gmail.com>
2025-01-06 03:28:19 +0300

    add redux

--- react-chat/src/index.js
+++ react-chat/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import AppRouter from './App'; 
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './AuthContext';
+import { Provider } from 'react-redux'; 
+import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <AppRouter />  
-  </AuthProvider>
+  <Provider store={store}> 
+    <AuthProvider>
+      <AppRouter />  
+    </AuthProvider>
+  </Provider>
 );
 
 reportWebVitals();
